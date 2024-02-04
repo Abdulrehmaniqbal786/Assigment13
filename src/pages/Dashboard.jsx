f6bc88babf5a2eb80dec2f1976c44b3c20f6791b
Abdulrehman Iqbal <abdullahiqbal83020@gmail.com>
2024-02-04 14:31:48 -0800

    Assigment13

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+    <h1>Dashboard</h1>
+    <p>Hi,Samantha. Welcome back to Sedap Admin!</p></>
+  )
+}
+
+export default Dashboard
